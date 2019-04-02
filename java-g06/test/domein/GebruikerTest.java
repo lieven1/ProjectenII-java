6e0414847801d479d5c4e0b8b27b39b7b92681d7
Bono Opalfvens <bono.opalfvens@student.hogent.be>
2019-04-02 09:30:58 +0200

    [Fix] JUnit packages

--- java-g06/test/domein/GebruikerTest.java
+++ java-g06/test/domein/GebruikerTest.java
@@ -1,26 +1,26 @@
 package domein;
 
 import java.util.Date;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class GebruikerTest {
     private Gebruiker g1;
     
-    @BeforeEach
+    @Before
     public void before() {
         g1 = new Gebruiker("Gebruiker1", "11111111111", new Date(2019, 3, 12), "John", "Doe", Geslacht.Man, new Date(2001, 8, 13), "Gent", "092432004", "0476585152", "gebruiker1@taijitan.be", "oudersGebruiker1@taijitan.be", new Adres("Belgie", "9000", "Gent", "Valentin Vaerwyckweg", "1"), 15, Gradatie.JuniDan, TypeGebruiker.Lid, new Lesformule(1, "Woensdag"));
     }
     
     @Test
     public void maakGebruiker_ValidGegevens() {
-        Assertions.assertEquals("Gebruiker", g1.getClass().getName());
+        Assert.assertEquals("Gebruiker", g1.getClass().getName());
     }
     
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void maakGebruiker_InvalidGegevens_throwsIllegalArgumentException() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> { 
+        Assert.assertThrows(IllegalArgumentException.class, () -> { 
             g1 = new Gebruiker(null, null, null, null, null, null, null, null, null, null, null, null, new Adres(null, null, null, null, null), 0, null, null, new Lesformule(0, null));
         });
     }

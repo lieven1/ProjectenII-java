f4adff6ca48232c3611f401e0b59f57f6fc741a6
Steve Kusters <steve.kusters@student.hogent.be>
2019-04-23 20:19:59 +0200

    Lesmateriaal domeinklassen

---
+++ java-g06/src/Lesmateriaal/Models/Thema.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lesmateriaal.Models;
+
+/**
+ *
+ * @author Steve
+ */
+class Thema {
+    
+    private String naam;
+
+    public Thema(String naam) {
+        this.naam = naam;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    
+    
+}
